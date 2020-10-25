00095d06922127068cafa9623f2948a7601fb3eb
mayidae <mayidae@gmail.com>
2020-10-25 15:51:59 -0430

    Modificacion El Souki de cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<-y
+    inv <<- NULL
+  }
+  get<-function(){x}
+  setInverse <- function(inverse) {inv <<-inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse =setInverse, getInverse = getInverse)
 }
 
+##Esta función crea un objeto que es una matriz y a su vez almacena en caché el calculo de su inversa.
 
 ## Write a short comment describing this function
+## Esta función calcula la inversa de la "matriz" que es devuelta por makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null (inv)){
+    message("geting cache data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
